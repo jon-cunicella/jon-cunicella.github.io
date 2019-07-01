06d707b495b00c9edacb79c864dcb3b395a92e03
jon-cunicella <joncunicella@gmail.com>
2019-07-01 16:47:52 -0400

    Colored header

--- dist/css.967bad95.js
+++ dist/css.967bad95.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65194" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65394" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
