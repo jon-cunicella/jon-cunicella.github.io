44d5f2e814ad8bc7bc1b15381d2368914de15789
jon-cunicella <joncunicella@gmail.com>
2019-07-02 11:10:56 -0400

    Added files for dropdown but not working

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,10 @@
-import pushBar from "./push-bar"
+function createDropdown(){
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav"){
+        x.className += " responsive";
+    }
+    else{
+        x.className = "topnav"
+    }
 
-pushBar.togglePushBar()
+}
